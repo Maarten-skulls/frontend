6c5a662d63886830195fbf872928d9a2d62030e7
Jasper Samson <s1136647@student.hsleiden.nl>
2024-04-30 20:27:31 +0200

    fitst commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Koulen } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const koulen = Koulen({
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['latin']
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={koulen.className}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      'bg-color': '#1C1818',
+    }
   },
   plugins: [],
 };
